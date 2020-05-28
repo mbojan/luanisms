3576c02dfbbf3b500501175b3ec73578ff5dc516
Michal Bojanowski <michal2992@gmail.com>
2020-05-28 23:09:54 +0200

    Factorial

---
+++ factorial.lua
@@ -0,0 +1,12 @@
+-- Define a factorial function
+function fact (n)
+	if n == 0 then
+		return 1
+	else 
+		return n * fact(n-1)
+	end
+end
+
+print("Enter a number:")
+a = io.read("*number") -- Read a number
+print(fact(a))
